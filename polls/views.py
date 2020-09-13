6c16870ea8aad817a775596511abf2c71305aab5
Rikuta Hiratsuka <rimuru930@gmail.com>
2020-09-14 01:34:13 +0900

    はじめての Django アプリ作成、その 3¶
    https://docs.djangoproject.com/ja/3.1/intro/tutorial03/

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello, world")
+
+
+def detail(request, question_id):
+    return HttpResponse("u r looking at question %s." % question_id)
+
+
+def result(request, question_id):
+    response = "u r looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('u r voting on question %s.' % question_id)
