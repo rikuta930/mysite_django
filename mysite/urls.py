baeec2093e4d496512a0b74c52c4a5772e439217
Rikuta Hiratsuka <rimuru930@gmail.com>
2020-09-13 21:35:32 +0900

    startapp polls

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import polls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('polls/', include(polls.urls))
 ]
